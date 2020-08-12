66d90b7773c29dc99b5de2b2c8b773c60a2502b0
Devansh Shukla <59922056+Devansh1352000@users.noreply.github.com>
2020-08-12 08:59:28 +0530

    Code to show nesting in For Loop.
    Added a code that includes 20 nested For loops.

---
+++ NestedForLoop.java
@@ -0,0 +1,71 @@
+/* Created By IntelliJ IDEA
+ *  Author: Devansh Shukla (Devansh1352000)
+ *  Date: 12-08-2020
+ *  Time: 08:43 AM
+ */
+
+public class NestedForLoop {
+    private static final int TEN = 10;
+    private static final int ZERO = 0;
+
+    public static void main(String[] args) {
+        for (int i1 = ZERO; i1 < TEN; i1++) {
+            for (int i2 = ZERO; i2 < TEN; i2++) {
+                for (int i3 = ZERO; i3 < TEN; i3++) {
+                    for (int i4 = ZERO; i4 < TEN; i4++) {
+                        for (int i5 = ZERO; i5 < TEN; i5++) {
+                            for (int i6 = ZERO; i6 < TEN; i6++) {
+                                for (int i7 = ZERO; i7 < TEN; i7++) {
+                                    for (int i8 = ZERO; i8 < TEN; i8++) {
+                                        for (int i9 = ZERO; i9 < TEN; i9++) {
+                                            for (int i10 = ZERO; i10 < TEN; i10++) {
+                                                for (int i11 = ZERO; i11 < TEN; i11++) {
+                                                    for (int i12 = ZERO; i12 < TEN; i12++) {
+                                                        for (int i13 = ZERO; i13 < TEN; i13++) {
+                                                            for (int i14 = ZERO; i14 < TEN; i14++) {
+                                                                for (int i15 = ZERO; i15 < TEN; i15++) {
+                                                                    for (int i16 = ZERO; i16 < TEN; i16++) {
+                                                                        for (int i17 = ZERO; i17 < TEN; i17++) {
+                                                                            for (int i18 = ZERO; i18 < TEN; i18++) {
+                                                                                for (int i19 = ZERO; i19 < TEN; i19++) {
+                                                                                    System.out.println(i19 + " ");
+                                                                                }
+                                                                                System.out.println();
+                                                                            }
+                                                                            System.out.println();
+                                                                        }
+                                                                        System.out.println();
+                                                                    }
+                                                                    System.out.println();
+                                                                }
+                                                                System.out.println();
+                                                            }
+                                                            System.out.println();
+                                                        }
+                                                        System.out.println();
+                                                    }
+                                                    System.out.println();
+                                                }
+                                                System.out.println();
+                                            }
+                                            System.out.println();
+                                        }
+                                        System.out.println();
+                                    }
+                                    System.out.println();
+                                }
+                                System.out.println();
+                            }
+                            System.out.println();
+                        }
+                        System.out.println();
+                    }
+                    System.out.println();
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
